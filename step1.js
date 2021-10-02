01d831a5648ed003b41d386559293c3183e0054e
Rob Birch <rbirchtree@gmail.com>
2021-10-02 11:57:09 -0500

    testing mozilla cookie tutorial

--- step1.js
+++ step1.js
@@ -1,3 +1,16 @@
 var intervalId = setInterval(function(){
 	console.log("Hello world")
 }, 10000);
+
+document.cookie = "name=oeschger; SameSite=None; Secure";
+document.cookie = "favorite_food=tripe; SameSite=None; Secure";
+
+function showCookies() {
+  const output = document.getElementById('cookies')
+  output.textContent = '> ' + document.cookie
+}
+
+function clearOutputCookies() {
+  const output = document.getElementById('cookies')
+  output.textContent = ''
+}
