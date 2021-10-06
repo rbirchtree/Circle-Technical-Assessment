aa9675462ade5f1e5c32bbc8a524cad1dea6daee
Rob Birch <rbirchtree@gmail.com>
2021-10-06 11:56:29 -0500

    cookie console log

--- step1.js
+++ step1.js
@@ -2,9 +2,11 @@ var intervalId = setInterval(function(){
 	console.log("Hello world")
 }, 10000);
 
+let tripValue = 0;
+
 document.cookie = "name=oeschger; SameSite=None; Secure";
 document.cookie = "favorite_food=tripe;SameSite=None; Secure";
-document.cookie = "trip_number="+ Math.random() + "; SameSite=None; Secure";
+document.cookie = "trip_number="+ tripValue + "; SameSite=None; Secure";
 
 function showCookies() {
   const output = document.getElementById('cookies')
@@ -15,3 +17,15 @@ function clearOutputCookies() {
   const output = document.getElementById('cookies')
   output.textContent = ''
 }
+
+if (window.performance) {
+  console.info("window.performance works fine on this browser");
+}
+console.info(performance.navigation.type);
+if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
+  console.info( "This page is reloaded" );
+} else {
+  console.info( "This page is not reloaded");
+}
+
+alert(pageAccessedByReload);
